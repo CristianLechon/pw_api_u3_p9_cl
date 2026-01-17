6cd273dce529d38bf1e6615b68cb5e15c7db4e1c
CristianLechon <calechon@uce.edu.ec>
2026-01-16 19:43:52 -0500

    Taller 26

--- src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
+++ src/main/java/uce/edu/web/api/matricula/application/EstudianteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import uce.edu.web.api.matricula.domain.Estudiante;
 import uce.edu.web.api.matricula.infraestructure.EstudianteRepository;
 
@@ -13,7 +14,46 @@ public class EstudianteService {
     @Inject
     private EstudianteRepository estudianteRepository;
 
-    public List<Estudiante> listarTodos(){
+    public List<Estudiante> listarTodos() {
         return this.estudianteRepository.listAll();
+    }
+
+    public Estudiante consultarPorId(Integer id) {
+        return this.estudianteRepository.findById(id.longValue());
+    }
+
+    @Transactional
+    public void crear(Estudiante estu) {
+        this.estudianteRepository.persist(estu);
+    }
+
+    @Transactional
+    public void actualizar(Integer id, Estudiante est){
+        Estudiante estu = consultarPorId(id);
+        estu.apellido = est.apellido;
+        estu.nombre = est.nombre;
+        estu.fechaNacimiento = est.fechaNacimiento;
+        // Se actualiza automaticamente por dirty cheking
+
+    }
+
+    @Transactional
+    public void actualizarParcial(Integer id, Estudiante est){
+        Estudiante estu = this.consultarPorId(id);
+
+        if (est.nombre != null) {
+            estu.nombre = est.nombre;
+        }
+        if (est.apellido != null) {
+            estu.apellido = est.apellido;
+        }
+        if (est.fechaNacimiento != null) {
+            estu.fechaNacimiento = est.fechaNacimiento;
+        }
+    }
+
+    @Transactional
+    public void eliminar(Integer id){
+        this.estudianteRepository.deleteById(id.longValue());
     }
 }
