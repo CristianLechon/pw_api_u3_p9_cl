352fbb22c37af63853c703bf3b56f78758ca6bbb
CristianLechon <calechon@uce.edu.ec>
2026-01-21 19:49:05 -0500

    Taller 28

--- src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
@@ -20,49 +20,49 @@ public class MateriaResource {
     private MateriaService materiaService;
 
     @GET
-    @Path("/todos")
+    @Path("")
     public List<Materia> listarTodos() {
         return this.materiaService.listarTodos();
     }
 
     @GET
-    @Path("/consultarPorId/{id}")
+    @Path("/{id}")
     public Materia consultarPorId(@PathParam("id") Long id){
         return this.materiaService.consultarPorId(id);
     }
 
     @POST
-    @Path("/crear")
+    @Path("")
     public void guardar(Materia materia) {
         this.materiaService.crear(materia);
     }
 
     @PUT
-    @Path("/actualizar/{id}")
+    @Path("/{id}")
     public void actualizar(@PathParam("id") Long id, Materia materia){
         this.materiaService.actualizar(materia, id);
     }
 
     @PATCH
-    @Path("/actualizarParcial/{id}")
+    @Path("/{id}")
     public void actualizarParcial(@PathParam("id") Long id, Materia materia){
         this.materiaService.actualizarParcial(materia, id);
     }
 
     @DELETE
-    @Path("/borrar/{id}")
+    @Path("/{id}")
     public void borrar(@PathParam("id") Long id){
         this.materiaService.eliminar(id);
     }
 
     @GET
-    @Path("/buscarPorNombre/{nombre}")
+    @Path("/nombre/{nombre}")
     public Materia buscarPorNombre(@PathParam("nombre") String nombre){
         return this.materiaService.buscarPorNombre(nombre);
     }
 
     @DELETE
-    @Path("/borrarPorSemestre/{semestre}")
+    @Path("/{semestre}")
     public Integer borrarPorSemestre(@PathParam("semestre") String semestre){
         return this.materiaService.borrarPorSemestre(semestre);
     }
